b631369c316f148c30190f00e44081fbd8287281
ismailhunaid <ismael552@gmail.com>
2021-03-21 16:24:11 +0500

    css repo added

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import Third from './Third'
+function App() {
+  return (
+    <div>
+      <div className="App">
+
+        <h1>Item One</h1>
+        <h2> Item Two</h2>
+
+
+      </div>
+      <div>
+        <Third />
+      </div>
+
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/Third.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Third() {
+    return (
+        <div>
+            <p> Third Item from another component</p> 
+            
+        </div>
+    )
+}
+
+export default Third
